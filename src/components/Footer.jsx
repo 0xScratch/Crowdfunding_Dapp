95d7ddcee78af1cf222f8973ffbb00bfc0961daa
Aryan Malik <aryanhunny98@gmail.com>
2024-01-03 23:19:23 +0530

    Started working on Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,22 @@
-export default function Footer() {
-    // Your component code
+import React from 'react'
+
+const Footer = () => {
+    const productList = ["Market", "ERC20 Token", "Donation"]
+    const contactList = [
+        "support@cryptoking.com",
+        "info@example.com",
+        "Contact Us"
+    ]
+
+    const usefulLink = ["Home", "About Us", "Company Bio"]
+
     return (
-        <div>Footer</div>
+        <footer className='text-center text-white backgroundMain lg:text-left'>
+            <div className='mx-6 py-10 text-center md:text-left'>
+                <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
+                    
+                </div>
+            </div>
+        </footer>
     )
 }
