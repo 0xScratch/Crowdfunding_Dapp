73c5e3861bc07336ef91677637961031d41999c7
Aryan Malik <aryanhunny98@gmail.com>
2024-01-04 12:56:29 +0530

    Implemented Footer Section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,9 +14,54 @@ const Footer = () => {
         <footer className='text-center text-white backgroundMain lg:text-left'>
             <div className='mx-6 py-10 text-center md:text-left'>
                 <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
-                    
+                    <div className=''>
+                        <h6 className='mb-4 flex items-center justify-center font-semibold uppercase md:justify-start'>
+                            Crypto King
+                        </h6>
+                        <p>
+                            An organization that plays a significant role in the development and promotion of various crypto projects. With a focus on innovation and cutting-edge technology, We aim to revolutionize the crypto industry by providing solutions and services that empower individuals and businesses.
+                        </p>
+                    </div>
+                    <div className=''>
+                        <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                            Products
+                        </h6>
+                        {productList.map((el, i) => (
+                            <p className='mb-4' key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div> 
+                    <div className=''>
+                        <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                            Useful links
+                        </h6>
+                        {usefulLink.map((el, i) => (
+                            <p className='mb-4' key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div>
+                    <div className=''>
+                        <h6 className='mb-4 flex justify-center font-semibold uppercase md:justify-start'>
+                            Contact
+                        </h6>
+                        {contactList.map((el, i) => (
+                            <p className='mb-4' key={i + 1}>
+                                <a href="#!">{el}</a>
+                            </p>
+                        ))}
+                    </div>   
                 </div>
+            </div>
+            <div className='backgroundMain p-6 text-center'>
+                <span>© 2023 Copyright: </span>
+                <a href="https://tailwind-elements.com/" className='font-semibold'>
+                     Crypto King
+                </a>
             </div>
         </footer>
     )
 }
+
+export default Footer
