674d968837b906e6f6201876a9d27061a92014e5
Aryan Malik <aryanhunny98@gmail.com>
2024-04-25 22:32:05 +0530

    Created Providers

--- src/app/providers.jsx
+++ src/app/providers.jsx
@@ -1,3 +1,19 @@
 'use client'
 
 import { DAppProvider, Sepolia } from "@usedapp/core"
+
+const config = {
+    readOnlyChainId: Sepolia.chainId,
+    readOnlyUrls: {
+        [Sepolia.chainId]: process.env.NEXT_PUBLIC_HTTPS
+    },
+    networks: [Sepolia],
+}
+
+export function Providers({children}) {
+    return (
+        <DAppProvider config={config}>
+            {children}
+        </DAppProvider>
+    )
+}
